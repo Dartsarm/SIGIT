acc043fcd251adb29cf24348106cf934ad3b2193
David Arturo Sarmiento Cardenas <Dsarca20@autofinanciera.local>
2025-11-21 10:02:13 -0500

    Ajustes de inicio y visualización de los forms

--- SIGIT/Controllers/AplicacionesController.cs
+++ SIGIT/Controllers/AplicacionesController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SIGIT.Models;
+using Microsoft.AspNetCore.Authorization; 
 
 namespace SIGIT.Controllers
 {
+    [Authorize]
     public class AplicacionesController : Controller
     {
         private readonly SigitContext _context;
@@ -27,70 +29,57 @@ namespace SIGIT.Controllers
         // GET: Aplicaciones/Details/5
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var aplicacione = await _context.Aplicaciones
-                .FirstOrDefaultAsync(m => m.AplicacionId == id);
-            if (aplicacione == null)
-            {
-                return NotFound();
-            }
+            var aplicacione = await _context.Aplicaciones.FirstOrDefaultAsync(m => m.AplicacionId == id);
+            if (aplicacione == null) return NotFound();
 
-            return View(aplicacione);
+            return PartialView(aplicacione);
         }
 
         // GET: Aplicaciones/Create
+        [Authorize(Roles = "Administrador")] // Solo el administrador puede crear
         public IActionResult Create()
         {
-            return View();
+            
+            return PartialView();
         }
 
         // POST: Aplicaciones/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> Create([Bind("AplicacionId,NombreAplicacion")] Aplicacione aplicacione)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(aplicacione);
                 await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                return Json(new { success = true });
             }
-            return View(aplicacione);
+            return PartialView(aplicacione);
         }
 
         // GET: Aplicaciones/Edit/5
+        [Authorize(Roles = "Administrador, Técnico")] // Admin y Técnico pueden editar
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
             var aplicacione = await _context.Aplicaciones.FindAsync(id);
-            if (aplicacione == null)
-            {
-                return NotFound();
-            }
-            return View(aplicacione);
+            if (aplicacione == null) return NotFound();
+
+            
+            return PartialView(aplicacione);
         }
 
         // POST: Aplicaciones/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador, Técnico")]
         public async Task<IActionResult> Edit(int id, [Bind("AplicacionId,NombreAplicacion")] Aplicacione aplicacione)
         {
-            if (id != aplicacione.AplicacionId)
-            {
-                return NotFound();
-            }
+            if (id != aplicacione.AplicacionId) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -101,41 +90,30 @@ namespace SIGIT.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!AplicacioneExists(aplicacione.AplicacionId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!AplicacioneExists(aplicacione.AplicacionId)) return NotFound();
+                    else throw;
                 }
-                return RedirectToAction(nameof(Index));
+                return Json(new { success = true });
             }
-            return View(aplicacione);
+            return PartialView(aplicacione);
         }
 
         // GET: Aplicaciones/Delete/5
+        [Authorize(Roles = "Administrador")] // Solo el administrador puede eliminar
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            if (id == null) return NotFound();
 
-            var aplicacione = await _context.Aplicaciones
-                .FirstOrDefaultAsync(m => m.AplicacionId == id);
-            if (aplicacione == null)
-            {
-                return NotFound();
-            }
+            var aplicacione = await _context.Aplicaciones.FirstOrDefaultAsync(m => m.AplicacionId == id);
+            if (aplicacione == null) return NotFound();
 
-            return View(aplicacione);
+            return PartialView(aplicacione); // Devuelve PartialView
         }
 
         // POST: Aplicaciones/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var aplicacione = await _context.Aplicaciones.FindAsync(id);
@@ -145,7 +123,7 @@ namespace SIGIT.Controllers
             }
 
             await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            return Json(new { success = true }); // Devuelve JSON para el script del modal
         }
 
         private bool AplicacioneExists(int id)
--- SIGIT/Controllers/AreasController.cs
+++ SIGIT/Controllers/AreasController.cs
@@ -2,13 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using SIGIT.Models;
+using System.Data.Common;
 
 namespace SIGIT.Controllers
 {
+    
+    [Authorize(Roles = "Administrador, Técnico, Usuario")]
     public class AreasController : Controller
     {
         private readonly SigitContext _context;
@@ -18,78 +21,60 @@ namespace SIGIT.Controllers
             _context = context;
         }
 
-        // GET: Areas
+        // GET: Areas (Todos pueden ver)
         public async Task<IActionResult> Index()
         {
             return View(await _context.Areas.ToListAsync());
         }
 
-        // GET: Areas/Details/5
+        // GET: Areas/Details/5 (Todos pueden ver)
         public async Task<IActionResult> Details(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
+            if (id == null) return NotFound();
             var area = await _context.Areas.FirstOrDefaultAsync(m => m.AreaId == id);
-            if (area == null)
-            {
-                return NotFound();
-            }
-
+            if (area == null) return NotFound();
             return PartialView(area);
         }
 
-        // GET: Areas/Create
+        // GET: Areas/Create (Solo Admin y Técnico)
+        [Authorize(Roles = "Administrador, Técnico")]
         public IActionResult Create()
         {
             return PartialView();
         }
 
-        // POST: Areas/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Areas/Create (Solo Admin y Técnico)
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador, Técnico")]
         public async Task<IActionResult> Create([Bind("AreaId,NombreArea")] Area area)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(area);
                 await _context.SaveChangesAsync();
-                return Json(new { success = true }); //RedirectToAction(nameof(Index)); ojo se debe tener en cuenta que devuelva el json en el return para que todo funcione bien
+                return Json(new { success = true }); // Devuelve JSON de éxito
             }
             return PartialView(area);
         }
 
-        // GET: Areas/Edit/5
+        // GET: Areas/Edit/5 (Admin y Técnico)
+        [Authorize(Roles = "Administrador, Técnico")]
         public async Task<IActionResult> Edit(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
+            if (id == null) return NotFound();
             var area = await _context.Areas.FindAsync(id);
-            if (area == null)
-            {
-                return NotFound();
-            }
+            if (area == null) return NotFound();
             return PartialView(area);
         }
 
-        // POST: Areas/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        // POST: Areas/Edit/5 (Admin y Técnico)
         [HttpPost]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador, Técnico")]
         public async Task<IActionResult> Edit(int id, [Bind("AreaId,NombreArea")] Area area)
         {
-            if (id != area.AreaId)
-            {
-                return NotFound();
-            }
+            if (id != area.AreaId) return NotFound();
 
             if (ModelState.IsValid)
             {
@@ -100,51 +85,54 @@ namespace SIGIT.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!AreaExists(area.AreaId))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
+                    if (!AreaExists(area.AreaId)) return NotFound();
+                    else throw;
                 }
-                return Json(new { success = true });  //RedirectToAction(nameof(Index)), ojo se debe tener en cuenta que devuelva el json en el return para que todo funcione bien
+                return Json(new { success = true }); // Devuelve JSON de éxito
             }
             return PartialView(area);
         }
 
-        // GET: Areas/Delete/5
+        // GET: Areas/Delete/5 (Solo Admin)
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> Delete(int? id)
         {
-            if (id == null)
-            {
-                return NotFound();
-            }
-
-            var area = await _context.Areas
-                .FirstOrDefaultAsync(m => m.AreaId == id);
-            if (area == null)
-            {
-                return NotFound();
-            }
-
-            return View(area);
+            if (id == null) return NotFound();
+            var area = await _context.Areas.FirstOrDefaultAsync(m => m.AreaId == id);
+            if (area == null) return NotFound();
+            return PartialView(area);
         }
 
         // POST: Areas/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Administrador")]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
             var area = await _context.Areas.FindAsync(id);
-            if (area != null)
-            {
-                _context.Areas.Remove(area);
-            }
 
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
+            try
+            {
+                if (area != null)
+                {
+                    _context.Areas.Remove(area);
+                    await _context.SaveChangesAsync();
+                }
+                return Json(new { success = true });
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is DbException dbEx && (dbEx.Message.Contains("REFERENCE constraint") || dbEx.Message.Contains("FK_")))
+                {
+                    return Json(new { success = false, message = "No se puede eliminar esta Área porque está siendo utilizada por uno o más empleados. Elimine o reasigne los empleados primero." });
+                }
+
+                return Json(new { success = false, message = $"Error de servidor inesperado: {ex.Message}" });
+            }
+            catch (Exception ex)
+            {
+                return Json(new { success = false, message = $"Error desconocido: {ex.Message}" });
+            }
         }
 
         private bool AreaExists(int id)
